94955952edcc6f7a436bfe2826fb03454c9c8c65
zxl2014 <zhanghan0031@163.com>
2018-11-02 10:15:06 +0800

    feature:add idea desktop

--- src/docs/asciidoc/linux/ubuntu/install-ide-idea.sh
+++ src/docs/asciidoc/linux/ubuntu/install-ide-idea.sh
@@ -31,12 +31,23 @@ rm -rf $download_dir/$ide_idea_filename
 aria2c -s 10 -x 10 -d $download_dir $ide_idea_url
 rm -rf $ide_dir/*idea*
 tar -xf $download_dir/$ide_idea_filename -C $ide_dir
+ide_idea_dir=$(find $ide_dir -maxdepth 1 -type d -iname 'idea*')
 
 # configure alias
 echo >> $HOME/.bash_aliases
-echo "alias idea='nohup "$(find $ide_dir -maxdepth 1 -type d -iname 'idea*')"/bin/idea.sh 1>/dev/null 2>&1 &'" >> $HOME/.bash_aliases
+echo "alias idea='nohup $ide_idea_dir/bin/idea.sh 1>/dev/null 2>&1 &'" >> $HOME/.bash_aliases
 source $HOME/.bash_aliases
 
-idea
+# add idea desktop
+echo "[Desktop Entry]
+Version=1.0
+Type=Application
+Name=IDEA Ultimate
+Icon=$ide_idea_dir/bin/idea.png
+Exec=\"$ide_idea_dir/bin/idea.sh\" %f
+Comment=Capable and Ergonomic IDE for JVM
+Categories=Development;IDE;
+Terminal=false
+StartupWMClass=jetbrains-idea" > $HOME/Desktop/jetbrains-idea.desktop
 
 echo 'install ide idea done'
