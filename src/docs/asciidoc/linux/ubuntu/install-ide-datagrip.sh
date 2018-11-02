f4a2df08a96bf901c0fb22587c620a10d83c6dc0
zxl2014 <zhanghan0031@163.com>
2018-11-02 10:09:27 +0800

    feature:add datagrip desktop

--- src/docs/asciidoc/linux/ubuntu/install-ide-datagrip.sh
+++ src/docs/asciidoc/linux/ubuntu/install-ide-datagrip.sh
@@ -27,16 +27,28 @@ mkdir -p $devtools_dir
 mkdir -p $ide_dir
 
 # download and tar
-datagrip_dir=$ide_dir/datagrip
 rm -rf $download_dir/$ide_datagrip_filename
 aria2c -s 10 -x 10 -d $download_dir $ide_datagrip_url
 tar -xf $download_dir/$ide_datagrip_filename -C $ide_dir
+datagrip_dir=$(find $ide_dir -maxdepth 1 -type d -iname 'datagrip*')
 
 # configure alias
 echo >> $HOME/.bash_aliases
-echo "alias datagrip='nohup "$(find $ide_dir -maxdepth 1 -type d -iname 'datagrip*')"/bin/datagrip.sh 1>/dev/null 2>&1 &'" >> $HOME/.bash_aliases
+echo "alias datagrip='nohup $datagrip_dir/bin/datagrip.sh 1>/dev/null 2>&1 &'" >> $HOME/.bash_aliases
 source $HOME/.bash_aliases
 
-datagrip
+# add datagrip desktop
+echo "[Desktop Entry]
+Version=1.0
+Type=Application
+Name=DataGrip
+Icon=$datagrip_dir/bin/datagrip.png
+Exec="$datagrip_dir/bin/datagrip.sh" %f
+Comment=IDE for Databases and SQL
+Categories=Development;IDE;
+Terminal=false
+StartupWMClass=jetbrains-datagrip" > $HOME/Desktop/jetbrains-datagrip.desktop
+
+chomd 755 $HOME/Desktop/datagrip.desktop
 
 echo 'install ide datagrip done'
